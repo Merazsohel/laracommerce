b64df144b8d518b09e768192569e92ab69e06958
devnet <merazhossain64@gmail.com>
2019-12-26 15:58:06 +0600

    admin login page modify

--- resources/views/back/auth/login.blade.php
+++ resources/views/back/auth/login.blade.php
@@ -1,85 +1,49 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Log in</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="{{ asset('public/back/bootstrap/css/bootstrap.min.css') }}">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('public/back/dist/css/AdminLTE.min.css') }}">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="{{ asset('public/back/plugins/iCheck/square/blue.css') }}">
-
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Admin Login</title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="stylesheet" href="{{ asset('public/back/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/back/dist/css/AdminLTE.min.css') }}">
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
-  <div class="login-logo">
-    <a href="../../index2.html"><b>Admin </b>Login</a>
-  </div>
-  <!-- /.auth-logo -->
-  <div class="login-box-body">
-    @if ($errors->has('email'))
-      <div class="alert alert-danger">
-        {{ $errors->first('email') }}
-      </div>
-    @endif
-    <p class="login-box-msg">Sign in to start your session</p>
+    <div class="login-logo">
+        <a href="#"><b>Admin </b>Login</a>
+    </div>
 
-    <form action="{{route('login')}}" method="post">
-      {{csrf_field()}}
-      <div class="form-group has-feedpublic/back">
-        <input type="email" class="form-control" name="email" placeholder="Email">
-        <span class="glyphicon glyphicon-envelope form-control-feedpublic/back"></span>
-      </div>
-      <div class="form-group has-feedpublic/back">
-        <input type="password" class="form-control" name="password" placeholder="Password">
-        <span class="glyphicon glyphicon-lock form-control-feedpublic/back"></span>
-      </div>
-      <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Remember Me
-            </label>
-          </div>
-        </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-        </div>
-        <!-- /.col -->
-      </div>
-    </form>
-    <!-- /.social-auth-links -->
+    <div class="login-box-body">
 
-    <a href="#">I forgot my password</a><br>
-  </div>
-  <!-- /.auth-box-body -->
+        @if ($errors->has('email'))
+            <div class="alert alert-danger">
+                {{ $errors->first('email') }}
+            </div>
+        @endif
+
+        <p class="login-box-msg"></p>
+
+        <form action="{{route('login')}}" method="post">
+            {{csrf_field()}}
+            <div class="form-group has-feedpublic/back">
+                <input type="email" class="form-control" name="email" placeholder="Email">
+
+            </div>
+            <div class="form-group has-feedpublic/back">
+                <input type="password" class="form-control" name="password" placeholder="Password">
+
+            </div>
+            <div class="row">
+                <div class="col-xs-4">
+                    <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                </div>
+            </div>
+        </form>
+
+    </div>
+
 </div>
-<!-- /.auth-box -->
 
-<!-- jQuery 2.2.3 -->
-<script src="{{ asset('public/back/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
-<!-- Bootstrap 3.3.6 -->
-<script src="{{ asset('public/back/bootstrap/js/bootstrap.min.js') }}"></script>
-<!-- iCheck -->
-<script src="{{ asset('public/back/plugins/iCheck/icheck.min.js') }}"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
-    });
-  });
-</script>
 </body>
 </html>
